076f89fc6157d73aed6b19eb10432d7b8aa54e0d
Binh.BuiDang <dangbinh4869@gmail.com>
2026-02-19 18:16:42 +0700

    add og/sitemap.

--- astro.config.ts
+++ astro.config.ts
@@ -4,11 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://friends.db99.dev",
+
   vite: {
     plugins: [tailwindcss()],
   },
 
   adapter: cloudflare(),
+  integrations: [sitemap()],
 });
